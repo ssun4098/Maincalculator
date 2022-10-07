ed43fbfe5c8b11fe608f5ee76d9191a39673c8e6
ssun4098 <86704576+ssun4098@users.noreply.github.com>
2022-10-07 15:22:50 +0900

    Delete Mul.java

--- Mul.java
+++
@@ -1,9 +0,0 @@
-public class Mul {
-    public static void main(String args[]) {
-        int a = 2;
-        int b = 3;
-        int result = a * b;
-        System.out.println("--- 2 곱하기 3 -----");
-        System.out.println("result");
-    }
-}
