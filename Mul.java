3668864a10e571a5368e192056e9c08c520db7c6
HwangTaeGyeong <htk5205@naver.com>
2022-10-07 15:00:45 +0900

    태경) 곱하기 추가

---
+++ Mul.java
@@ -0,0 +1,9 @@
+public class Mul {
+    public static void main(String args[]) {
+        int a = 2;
+        int b = 3;
+        int result = a * b;
+        System.out.println("--- 2 곱하기 3 -----");
+        System.out.println("result");
+    }
+}
